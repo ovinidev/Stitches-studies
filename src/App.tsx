bb0980709243b0ebc08f70b4a2a879c9a64ab31b
vinimedeiros13 <vinimedeiros7@outlook.com>
2022-02-07 13:44:54 -0300

    Testing styled components

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import { BrowserRouter as Router } from "react-router-dom";
 import styled from 'styled-components';
 
 const Title = styled.h1`
-  
+  color: red;
 `
 function App() {
 
   return (
     <Router>
       <MainRoutes />
+      <Title>Oi</Title>
     </Router>
   )
 }
