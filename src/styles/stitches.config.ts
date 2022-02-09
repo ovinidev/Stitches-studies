d105918be04bbf85c15b60446a823cb553772b28
vinimedeiros13 <vinimedeiros7@outlook.com>
2022-02-09 10:31:52 -0300

    Test

--- src/styles/stitches.config.ts
+++ src/styles/stitches.config.ts
@@ -12,45 +12,82 @@ export const {
 } = createStitches({
   utils: {
     // Abbreviated margin properties
-    m: (value) => ({
+    m: (value: string | number) => ({
       margin: value,
     }),
-    mt: (value) => ({
+    mt: (value: string | number) => ({
       marginTop: value,
     }),
-    mr: (value) => ({
+    mr: (value: string | number) => ({
       marginRight: value,
     }),
-    mb: (value) => ({
+    mb: (value: string | number) => ({
       marginBottom: value,
     }),
-    ml: (value) => ({
+    ml: (value: string | number) => ({
       marginLeft: value,
     }),
-    mx: (value) => ({
+    mx: (value: string | number) => ({
       marginLeft: value,
       marginRight: value,
     }),
-    my: (value) => ({
+    my: (value: string | number) => ({
       marginTop: value,
       marginBottom: value,
     }),
 
     // A property for applying width/height together
-    size: (value) => ({
+    size: (value: string | number) => ({
       width: value,
       height: value,
     }),
 
     // A property to apply linear gradient
-    linearGradient: (value) => ({
+    linearGradient: (value: string | number) => ({
       backgroundImage: `linear-gradient(${value})`,
     }),
 
     // An abbreviated property for border-radius
-    br: (value) => ({
+    br: (value: string | number) => ({
       borderRadius: value,
     }),
   },
-
+  theme: {
+    colors: {
+      gray500: '',
+      blue500: '',
+      purple500: '',
+      green500: '',
+      red500: '',
+    },
+    space: {
+      1: '5px',
+      2: '10px',
+      3: '15px',
+    },
+    fontSizes: {
+      1: '12px',
+      2: '13px',
+      3: '15px',
+    },
+    fonts: {
+      untitled: 'Untitled Sans, apple-system, sans-serif',
+      mono: 'Söhne Mono, menlo, monospace',
+    },
+    media: {
+      bp1: '(min-width: 640px)',
+      bp2: '(min-width: 768px)',
+      bp3: '(min-width: 1024px)',
+    },
+    fontWeights: {},
+    lineHeights: {},
+    letterSpacings: {},
+    sizes: {},
+    borderWidths: {},
+    borderStyles: {},
+    radii: {},
+    shadows: {},
+    zIndices: {},
+    transitions: {},
+  }
 });
